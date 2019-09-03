f4314dd61e3cb20bf1c78ab8576b177defc130ef
renatojobal <renatojobal@gmail.com>
2019-09-03 01:00:48 -0500

    Creé el proxy y le agregué nuevos tributos, espero que funcione

--- playing_with_django/myapp/models.py
+++ playing_with_django/myapp/models.py
@@ -1,3 +1,19 @@
-from django.db import models
 
-# Create your models here.
+# Django
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Modelo Proxy de usuario
+class ProxyUser(User):
+
+    # Ver si le puedo agregar nuevos atributos
+    birthday = models.DateField()
+    genre = models.CharField(max_length=1, blank=True)
+    points = models.IntegerField(blank=True)
+
+
+    # Indicandole que es un proxy
+    class Meta:
+        proxy = True
+
