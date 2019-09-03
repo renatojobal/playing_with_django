dbc6769c0ce6434647147106646a95e7f7e5bebe
renatojobal <renatojobal@gmail.com>
2019-09-03 01:40:14 -0500

    Si funciona! Par poder agregar más atributos al modelo de usuario es preferible  heredar

--- playing_with_django/myapp/admin.py
+++ playing_with_django/myapp/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import UserChild
 
+@admin.register(UserChild)
+class UserChild(admin.ModelAdmin):
 
-admin.site.register(UserChild)
+    list_display = ('pk', 'username', 'birthday',)
+    list_display_links = ('pk', 'username',)
+    list_editable = ('birthday',)
+
+    #search_fields = ()
+
+    #list_filter = ()
