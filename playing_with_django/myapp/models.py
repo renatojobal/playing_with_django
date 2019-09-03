446386f1fa80bf4486deb0db7c9633d0a38d9db8
renatojobal <renatojobal@gmail.com>
2019-09-03 01:10:26 -0500

    Creadno el usuario heredado de AbstracUser

--- playing_with_django/myapp/models.py
+++ playing_with_django/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Heredando de AbstracUser
+class UserChild(AbstractUser):
+
+    # Ver si le puedo agregar nuevos atributos
+    birthday = models.DateField()
+    genre = models.CharField(max_length=1, blank=True)
+    points = models.IntegerField(blank=True)
